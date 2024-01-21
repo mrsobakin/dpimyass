e33878fe81111f418006e8e578cf37049af7eab7
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2024-01-21 19:48:16 +0300

    Refactor, transition from PoC to a more usable state

---
+++ README.md
@@ -0,0 +1,50 @@
+# 🖕🕵 DPIMyAss
+
+DPIMyAss is a simple UDP proxy designed for bypassing DPI with close-to-zero overhead.
+
+## Why? 🤔
+
+I made this proxy to restore the wireguard functionality in places where it was blocked. DPIMyAss is way simpler than the other solutions like, say, shadowsocks, and it does not require messing with the ip interfaces to get it running. All you have to do to set it up with wireguard is run this proxy on both your server and client, and change the endpoint to local proxy address in your wireguard config.
+
+DPIMyAss also does not create any additional overhead on the network. The forwarded packets stay the exact same size they were, and no new packets are created.
+
+## Features 🚀
+
+- **UDP Obfuscation:** DPIMyAss mangles packets, making the underlying protocol unrecognizable to the DPI.
+- **Simplicity:** DPIMyAss is extremely simple and small. It's not trying to be what it isn't - there are no custom protocols or complex encryption here.
+- **Speed:** DPIMyAss uses simple XOR encryption, which results in almost zero processing overhead.
+
+## Getting Started 🛠️
+
+These instructions will help you set up and run DPIMyAss on your local machine and server.
+
+### Usage 🚦
+
+1. Clone this repo and `cd` into it
+2. Build the project:
+```bash
+cargo build --release
+```
+3. Run DPIMyAss:
+```bash
+./target/build/dpimyass [config.toml]
+```
+
+### Configuration ⚙️
+
+DPIMyAss uses a TOML configuration file to specify its settings. Below is an example configuration:
+
+```toml
+[proxy]
+address = "127.0.0.1:1337"
+buffer = 16384
+timeout = 60
+
+[downstream]
+address = "endpoint.example.com:8888"
+buffer = 16384
+timeout = 60
+
+[obfuscation]
+key = [239, 42, 13, 69]
+```
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,98 @@
-use std::{error::Error, net::SocketAddr, str::FromStr, time::Duration};
+#![feature(trait_alias)]
+
+use std::{
+    error::Error,
+    net::SocketAddr,
+    net::ToSocketAddrs,
+    time::Duration
+};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncRead, AsyncWrite},
     time::timeout,
 };
-use udp_stream::UdpStream;
+use serde::Deserialize;
+use serde::de::Deserializer;
+use serde_with::DurationSeconds;
+use serde_with::serde_as;
+use udp_stream::{UdpListener, UdpStream};
 
-const UDP_BUFFER_SIZE: usize = 16 * 1024;
-const UDP_TIMEOUT: Duration = Duration::from_secs(60);
-const XOR_KEY: [u8; 3] = [69, 42, 239];
 
-fn obfuscate(buf: &mut [u8]) {
-    for (bi, ki) in buf.iter_mut().zip(XOR_KEY.iter().cycle()) {
-        *bi ^= ki
+fn resolve_address<'de, D>(de: D) -> Result<SocketAddr, D::Error>
+where D: Deserializer<'de> {
+    let addr = <String>::deserialize(de)?;
+
+    addr.to_socket_addrs()
+       .map_err(serde::de::Error::custom)?
+       .next()
+       .ok_or(serde::de::Error::custom("No address"))
+}
+
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    proxy: EndpointConfig,
+    downstream: EndpointConfig,
+    obfuscation: Obfuscation,
+}
+
+#[serde_as]
+#[derive(Deserialize, Debug)]
+struct EndpointConfig {
+    #[serde(deserialize_with = "resolve_address")]
+    address: SocketAddr,
+    buffer: usize,
+    #[serde_as(as = "DurationSeconds<u64>")]
+    timeout: Duration,
+}
+
+#[derive(Deserialize, Debug)]
+struct Obfuscation {
+    key: Vec<u8>,
+}
+
+
+fn xor_obfuscate(data: &mut [u8], key: &[u8]) {
+    for (di, ki) in data.iter_mut().zip(key.iter().cycle()) {
+        *di ^= ki
     }
 }
 
-async fn forward_loop(mut upstream: UdpStream, mut downstream: UdpStream) -> Result<(), std::io::Error> {
-    let mut upbuf = vec![0u8; UDP_BUFFER_SIZE];
-    let mut downbuf = vec![0u8; UDP_BUFFER_SIZE];
+
+trait Stream = AsyncWrite + AsyncRead + Unpin;
+
+struct StreamAndInfo<S: Stream> {
+    stream: S,
+    buffer: usize,
+    timeout: Duration
+}
+
+impl<S: Stream> StreamAndInfo<S> {
+    fn new(stream: S, config: &EndpointConfig) -> Self {
+        StreamAndInfo {
+            stream,
+            buffer: config.buffer,
+            timeout: config.timeout,
+        }
+    }
+}
+
+
+async fn forward_loop(mut upstream: StreamAndInfo<impl Stream>, mut downstream: StreamAndInfo<impl Stream>, key: &[u8]) -> Result<(), std::io::Error> {
+    let mut upbuf = vec![0u8; upstream.buffer];
+    let mut downbuf = vec![0u8; downstream.buffer];
 
     loop {
         tokio::select! {
-            n = timeout(UDP_TIMEOUT, upstream.read(&mut upbuf)) => {
+            n = timeout(upstream.timeout, upstream.stream.read(&mut upbuf)) => {
                 let n = n??;
-                println!("Incoming from upstream:\n{:?}\n", &upbuf[0..n]);
-                obfuscate(&mut upbuf);
-                downstream.write_all(&upbuf[0..n]).await?;
+                xor_obfuscate(&mut upbuf[0..n], key);
+                downstream.stream.write_all(&upbuf[0..n]).await?;
             },
-            n = timeout(UDP_TIMEOUT, downstream.read(&mut downbuf)) => {
+            n = timeout(downstream.timeout, downstream.stream.read(&mut downbuf)) => {
                 let n = n??;
-                println!("Incoming from downstream:\n{:?}\n", &downbuf[0..n]);
-                obfuscate(&mut downbuf);
-                upstream.write_all(&downbuf[0..n]).await?;
+                xor_obfuscate(&mut downbuf[0..n], key);
+                upstream.stream.write_all(&downbuf[0..n]).await?;
             }
         };
     }
@@ -40,23 +101,31 @@ async fn forward_loop(mut upstream: UdpStream, mut downstream: UdpStream) -> Res
     Ok::<(), std::io::Error>(())
 }
 
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let bind_addr = std::env::args().nth(1).expect("no bind address given");
-    let bind_addr = SocketAddr::from_str(&bind_addr)?;
+    let config: &Config = Box::leak({
+        let config_file = std::env::args().nth(1).unwrap_or_else(|| "config.toml".to_owned());
 
-    let downstream_addr = std::env::args().nth(2).expect("no downstream address given");
-    let downstream_addr = SocketAddr::from_str(&downstream_addr)?;
+        let s = std::fs::read_to_string(config_file)?;
+        toml::from_str(&s)?
+    });
 
-    let listener = udp_stream::UdpListener::bind(bind_addr).await?;
+    let listener = UdpListener::bind(config.proxy.address).await?;
+
+    println!("Listening on {:?}, downstream {:?}", config.proxy.address, config.downstream.address);
 
     loop {
-        let (upstream, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            let downstream = udp_stream::UdpStream::connect(downstream_addr).await?;
+        let (upstream, addr) = listener.accept().await?;
+        let upstream = StreamAndInfo::new(upstream, &config.proxy);
 
-            if let Err(e) = forward_loop(upstream, downstream).await {
-                println!("Error: {e:?}");
+        println!("New incoming connection from {addr:?}");
+
+        tokio::spawn(async move {
+            let downstream = StreamAndInfo::new(UdpStream::connect(config.downstream.address).await?, &config.downstream);
+
+            if let Err(e) = forward_loop(upstream, downstream, &config.obfuscation.key).await {
+                println!("Error: {e:?} ({addr:?})");
             }
 
             Ok::<(), std::io::Error>(())
